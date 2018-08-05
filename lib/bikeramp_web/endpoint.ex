3db640bc5be9fd39a96fe66d279b10e6b5ea02cb
Bartosz Kalinowski <kelostrada@gmail.com>
2018-08-05 21:53:52 +0200

    Remove channels support

--- lib/bikeramp_web/endpoint.ex
+++ lib/bikeramp_web/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule BikerampWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :bikeramp
 
-  socket "/socket", BikerampWeb.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
