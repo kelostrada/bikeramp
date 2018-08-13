c5837541455e8b961dacd2caf7df473c1333fdd0
Bartosz Kalinowski <kelostrada@gmail.com>
2018-08-13 20:55:21 +0200

    Don’t debug deploy.sh anymore

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo $GIGALIXIR_EMAIL
-echo $GIGALIXIR_APP_NAME
 git remote add gigalixir https://$GIGALIXIR_EMAIL:$GIGALIXIR_API_KEY@git.gigalixir.com/$GIGALIXIR_APP_NAME.git
 git push -f gigalixir HEAD:refs/heads/master
 
